38aa6922e39b9ca86f7f716ba9ed2a06b2def159
Eddy Jo <edwardjo811@gmail.com>
2018-12-01 14:16:37 -0500

    add method to js

--- public/js/board.js
+++ public/js/board.js
@@ -1,45 +1,59 @@
+var x = "X";
+var o = "O";
+
+var shape = "X";
+
+function shapeTurn() {
+  if (shape == "X") {
+    shape = "O";
+  } else {
+    shape = "X";
+  }
+  return shape;
+}
+
 
 function myFunction0() {
-    document.getElementById("0").innerHTML = "X";
+    document.getElementById("0").innerHTML = shapeTurn();
     document.getElementById("0").disabled = true;
 }
 
 function myFunction1() {
-    document.getElementById("1").innerHTML = "X";
+    document.getElementById("1").innerHTML = shapeTurn();
     document.getElementById("1").disabled = true;
 }
 
 function myFunction2() {
-    document.getElementById("2").innerHTML = "X";
+    document.getElementById("2").innerHTML = shapeTurn();
     document.getElementById("2").disabled = true;
 }
 
 function myFunction3() {
-    document.getElementById("3").innerHTML = "X";
+    document.getElementById("3").innerHTML = shapeTurn();
     document.getElementById("3").disabled = true;
 }
 
 function myFunction4() {
-    document.getElementById("4").innerHTML = "X";
+    document.getElementById("4").innerHTML = shapeTurn();
     document.getElementById("4").disabled = true;
 }
 
 function myFunction5() {
-    document.getElementById("5").innerHTML = "X";
+    document.getElementById("5").innerHTML = shapeTurn();
     document.getElementById("5").disabled = true;
 }
 
 function myFunction6() {
-    document.getElementById("6").innerHTML = "X";
+    document.getElementById("6").innerHTML = shapeTurn();
     document.getElementById("6").disabled = true;
 }
 
 function myFunction7() {
-    document.getElementById("7").innerHTML = "X";
+    document.getElementById("7").innerHTML = shapeTurn();
     document.getElementById("7").disabled = true;
 }
 
 function myFunction8() {
-    document.getElementById("8").innerHTML = "X";
+    document.getElementById("8").innerHTML = shapeTurn();
     document.getElementById("8").disabled = true;
 }

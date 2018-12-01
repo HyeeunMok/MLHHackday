c3ae188e267ef921e60bcc50d451959ef9dd73c1
Sraavan <sraavansridhar@gmail.com>
2018-12-01 14:03:19 -0500

    Updated static directory path

--- server/server.js
+++ server/server.js
@@ -3,14 +3,19 @@ const server = require('./config/headers').server
 const express = require('./config/headers').express
 const port = process.env.PORT || 3000;
 var path = require('path')
-
+var bodyParser = require('body-parser')
 var socketManager = require('./controllers/socketManager')
-app.use('/',express.static(path.join(__dirname,'public')))
 
-server.listen(port,() => {
+app.use('/', express.static(path.join(__dirname,'..', 'public')))
+
+// body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+server.listen(port, () => {
     console.log(`Server listening on http://localhost:${port}`);
 })
 
-app.get('/', (req,res) =>{
-    res.sendFile(path.join(__dirname,"..","public","index.html"))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "public", "index.html"))
 })
